8e56a4833e4579530ee9020aa5ef1c5a9eccbbf1
Ilya Kardapolov <kardapolov@gmail.com>
2024-12-10 11:57:03 +0500

    Menu microservice

--- init-db.sql
+++ init-db.sql
@@ -1,7 +1,9 @@
 CREATE DATABASE IF NOT EXISTS `restpilot-auth-dev`;
 CREATE DATABASE IF NOT EXISTS `restpilot-orders-dev`;
+CREATE DATABASE IF NOT EXISTS `restpilot-menu-dev`;
 
 ALTER USER 'root'@'%' IDENTIFIED BY 'root';
 GRANT ALL PRIVILEGES ON `restpilot-auth-dev`.* TO 'root'@'%';
 GRANT ALL PRIVILEGES ON `restpilot-orders-dev`.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON `restpilot-menu-dev`.* TO 'root'@'%';
 FLUSH PRIVILEGES;
