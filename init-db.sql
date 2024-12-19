2ec1838d123591a5938ed6a5f282485def0f2350
Ilya Kardapolov <kardapolov@gmail.com>
2024-12-19 12:14:46 +0500

    Updated submodules to latest commits

--- init-db.sql
+++ init-db.sql
@@ -1,9 +1,11 @@
 CREATE DATABASE IF NOT EXISTS `restpilot-auth-dev`;
 CREATE DATABASE IF NOT EXISTS `restpilot-orders-dev`;
 CREATE DATABASE IF NOT EXISTS `restpilot-menu-dev`;
+CREATE DATABASE IF NOT EXISTS `restpilot-companies-dev`;
 
 ALTER USER 'root'@'%' IDENTIFIED BY 'root';
 GRANT ALL PRIVILEGES ON `restpilot-auth-dev`.* TO 'root'@'%';
 GRANT ALL PRIVILEGES ON `restpilot-orders-dev`.* TO 'root'@'%';
 GRANT ALL PRIVILEGES ON `restpilot-menu-dev`.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON `restpilot-companies-dev`.* TO 'root'@'%';
 FLUSH PRIVILEGES;
